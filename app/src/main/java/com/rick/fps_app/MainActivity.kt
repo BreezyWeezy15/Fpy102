5cce94795b5719bed352d6df8bd04a0db8ab2880
DESKTOP-JJ86S6U\BreezyWeezy <eddinetaki087@gmail.com>
2024-12-06 19:52:43 +0100

    new changes

--- app/src/main/java/com/rick/fps_app/MainActivity.kt
+++ app/src/main/java/com/rick/fps_app/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Handler
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.rick.fps_app.databinding.ActivityMainBinding
 import kotlinx.coroutines.Runnable
@@ -30,7 +32,11 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
 
         // FPS
